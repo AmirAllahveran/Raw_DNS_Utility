4bbb7e42739d17fee1ff46c17d7dfa0ec2be0ed8
amir4141-creator <53536617+amir4141-creator@users.noreply.github.com>
2021-04-15 12:31:15 +0430

    complete

--- main.py
+++ main.py
@@ -1,107 +1,96 @@
-import binascii
-import socket
-
-
-def create_packet(address=""):
-    RD = 0
-    params = "{:04x}".format(
-        int(str(0) + str(0).zfill(4) + str(0) + str(0) + str(RD) + str(0) + str(0).zfill(3) + str(0).zfill(4), 2))
-
-    packet = ""
-    packet += "{:04x}".format(43690)
-    packet += params
-    packet += "{:04x}".format(1)
-    packet += "{:04x}".format(0)
-    packet += "{:04x}".format(0)
-    packet += "{:04x}".format(0)
-
-    addr_parts = address.split(".")
-    for part in addr_parts:
-        addr_len = "{:02x}".format(len(part))
-        addr_part = binascii.hexlify(part.encode())
-        packet += addr_len
-        packet += addr_part.decode()
-
-    packet += "00"
-    packet += "{:04x}".format(1)
-    packet += "{:04x}".format(1)
-
-    return packet
-
-
-def decode_packet(message):
-    res = []
-
-    QDCOUNT = message[8:12]
-    ANCOUNT = message[12:16]
-    NSCOUNT = message[16:20]
-    ARCOUNT = message[20:24]
-
-    # Question section
-    QUESTION_SECTION_STARTS = 24
-    question_parts = parse_parts(message, QUESTION_SECTION_STARTS, [])
-
-    QTYPE_STARTS = QUESTION_SECTION_STARTS + (len("".join(question_parts))) + (len(question_parts) * 2) + 2
-    QCLASS_STARTS = QTYPE_STARTS + 4
-
-    # Answer section
-    ANSWER_SECTION_STARTS = QCLASS_STARTS + 4
-
-    NUM_ANSWERS = max([int(ANCOUNT, 16), int(NSCOUNT, 16), int(ARCOUNT, 16)])
-    if NUM_ANSWERS > 0:
-        for ANSWER_COUNT in range(NUM_ANSWERS):
-            if ANSWER_SECTION_STARTS < len(message):
-                ATYPE = message[ANSWER_SECTION_STARTS + 4:ANSWER_SECTION_STARTS + 8]
-                RDLENGTH = int(message[ANSWER_SECTION_STARTS + 20:ANSWER_SECTION_STARTS + 24], 16)
-                RDDATA = message[ANSWER_SECTION_STARTS + 24:ANSWER_SECTION_STARTS + 24 + (RDLENGTH * 2)]
-
-                if ATYPE == "{:04x}".format(1):
-                    octets = [RDDATA[i:i + 2] for i in range(0, len(RDDATA), 2)]
-                    RDDATA_decoded = ".".join(list(map(lambda x: str(int(x, 16)), octets)))
-                else:
-                    RDDATA_decoded = ".".join(
-                        map(lambda p: binascii.unhexlify(p).decode('iso8859-1'), parse_parts(RDDATA, 0, [])))
-
-                ANSWER_SECTION_STARTS = ANSWER_SECTION_STARTS + 24 + (RDLENGTH * 2)
-
-            try:
-                ATYPE
-            except NameError:
-                None
-            else:
-                res.append("# ANSWER " + str(ANSWER_COUNT + 1) + ":" + RDDATA_decoded)
-
-    return "\n".join(res)
-
-
-
-
-def parse_parts(message, start, parts):
-    part_start = start + 2
-    part_len = message[start:part_start]
-
-    if len(part_len) == 0:
-        return parts
-
-    part_end = part_start + (int(part_len, 16) * 2)
-    parts.append(message[part_start:part_end])
-
-    if message[part_end:part_end + 2] == "00" or part_end > len(message):
-        return parts
-    else:
-        return parse_parts(message, part_end, parts)
-
-
-if __name__ == '__main__':
-    url = input("Enter your name address:")
-    message = create_packet(url)
-    message = message.replace(" ", "").replace("\n", "")
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        sock.sendto(binascii.unhexlify(message), ("198.41.0.4", 53))
-        response, _ = sock.recvfrom(4096)
-    finally:
-        sock.close()
-
-    print("Response:\n" + decode_packet(binascii.hexlify(response).decode("utf-8")))
+# import binascii
+# import socket
+#
+#
+# def create_packet(address=""):
+#     RD = 1
+#     params = "{:04x}".format(
+#         int(str(0) + str(0).zfill(4) + str(0) + str(0) + str(RD) + str(0) + str(0).zfill(3) + str(0).zfill(4), 2))
+#
+#     packet = ""
+#     packet += "{:04x}".format(43690)
+#     packet += params
+#     packet += "{:04x}".format(1)
+#     packet += "{:04x}".format(0)
+#     packet += "{:04x}".format(0)
+#     packet += "{:04x}".format(0)
+#
+#     addr_parts = address.split(".")
+#     for part in addr_parts:
+#         addr_len = "{:02x}".format(len(part))
+#         addr_part = binascii.hexlify(part.encode())
+#         packet += addr_len
+#         packet += addr_part.decode()
+#
+#     packet += "00"
+#     packet += "{:04x}".format(1)
+#     packet += "{:04x}".format(1)
+#
+#     return packet
+#
+#
+# def decode_packet(packet):
+#     res = []
+#
+#     ANCOUNT = packet[12:16]
+#     NSCOUNT = packet[16:20]
+#     ARCOUNT = packet[20:24]
+#
+#     answer = 32 + (len("".join(parse_parts(packet, 24, [])))) + (len(parse_parts(packet, 24, [])) * 2) + 2
+#
+#     answer_number = max([int(ANCOUNT, 16), int(NSCOUNT, 16), int(ARCOUNT, 16)])
+#     if answer_number > 0:
+#         for i in range(answer_number):
+#             if answer < len(packet):
+#                 ATYPE = packet[answer + 4:answer + 8]
+#                 RDLENGTH = int(packet[answer + 20:answer + 24], 16)
+#                 RDDATA = packet[answer + 24:answer + 24 + (RDLENGTH * 2)]
+#
+#                 if ATYPE == "{:04x}".format(1):
+#                     fetchedBytes = [RDDATA[i:i + 2] for i in range(0, len(RDDATA), 2)]
+#                     data = ".".join(list(map(lambda x: str(int(x, 16)), fetchedBytes)))
+#                 else:
+#                     data = ".".join(
+#                         map(lambda p: binascii.unhexlify(p).decode('iso8859-1'), parse_parts(RDDATA, 0, [])))
+#
+#                 answer = answer + 24 + (RDLENGTH * 2)
+#
+#             try:
+#                 ATYPE
+#             except NameError:
+#                 None
+#             else:
+#                 res.append("# " + str(i + 1) + " : " + data)
+#
+#     return res
+#
+#
+# def parse_parts(message, start, parts):
+#     part_start = start + 2
+#     part_len = message[start:part_start]
+#
+#     if len(part_len) == 0:
+#         return parts
+#
+#     part_end = part_start + (int(part_len, 16) * 2)
+#     parts.append(message[part_start:part_end])
+#
+#     if message[part_end:part_end + 2] == "00" or part_end > len(message):
+#         return parts
+#     else:
+#         return parse_parts(message, part_end, parts)
+#
+#
+# if __name__ == '__main__':
+#     url = input("Enter your name address:")
+#     message = create_packet(url)
+#     message = message.replace(" ", "").replace("\n", "")
+#
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     try:
+#         sock.sendto(binascii.unhexlify(message), ("8.8.8.8", 53))
+#         response, _ = sock.recvfrom(4096)
+#     finally:
+#         sock.close()
+#
+#     print(decode_packet(binascii.hexlify(response).decode("utf-8")))
--- part4/DnsIterativeResolver.py
+++ part4/DnsIterativeResolver.py
@@ -3,7 +3,7 @@ import socket
 
 
 def create_packet(address=""):
-    RD = 0
+    RD = 1
     params = "{:04x}".format(
         int(str(0) + str(0).zfill(4) + str(0) + str(0) + str(RD) + str(0) + str(0).zfill(3) + str(0).zfill(4), 2))
 
@@ -31,54 +31,54 @@ def create_packet(address=""):
 
 def decode_packet(packet):
     res = []
+
+    ANCOUNT = packet[12:16]
     NSCOUNT = packet[16:20]
-    print("NSCOUNT: " + str(int(NSCOUNT, 16)))
+    ARCOUNT = packet[20:24]
 
-    question = parse_parts(packet, 24, [])
+    answer = 32 + (len("".join(parse_parts(packet, 24, [])))) + (len(parse_parts(packet, 24, [])) * 2) + 2
 
-    Answer = 34 + (len("".join(question))) + (len(question) * 2)
-
-    NUM_NAME_SERVER = int(NSCOUNT, 16)
-    if NUM_NAME_SERVER > 0:
-        for count in range(NUM_NAME_SERVER):
-            if Answer < len(packet):
-                ATYPE = packet[Answer + 4:Answer + 8]
-                RDLENGTH = int(packet[Answer + 20:Answer + 24], 16)
-                RDDATA = packet[Answer + 24:Answer + 24 + (RDLENGTH * 2)]
+    answer_number = max([int(ANCOUNT, 16), int(NSCOUNT, 16), int(ARCOUNT, 16)])
+    if answer_number > 0:
+        for i in range(answer_number):
+            if answer < len(packet):
+                ATYPE = packet[answer + 4:answer + 8]
+                RDLENGTH = int(packet[answer + 20:answer + 24], 16)
+                RDDATA = packet[answer + 24:answer + 24 + (RDLENGTH * 2)]
 
                 if ATYPE == "{:04x}".format(1):
-                    octets = [RDDATA[i:i + 2] for i in range(0, len(RDDATA), 2)]
-                    data = ".".join(list(map(lambda x: str(int(x, 16)), octets)))
+                    fetchedBytes = [RDDATA[i:i + 2] for i in range(0, len(RDDATA), 2)]
+                    data = ".".join(list(map(lambda x: str(int(x, 16)), fetchedBytes)))
                 else:
                     data = ".".join(
                         map(lambda p: binascii.unhexlify(p).decode('iso8859-1'), parse_parts(RDDATA, 0, [])))
 
-                Answer = Answer + 24 + (RDLENGTH * 2)
+                answer = answer + 24 + (RDLENGTH * 2)
 
             try:
                 ATYPE
             except NameError:
                 None
             else:
-                res.append("name server " + str(count) + ": " + data)
+                res.append("# " + str(i + 1) + " : " + data)
 
-    return "\n".join(res)
+    return res
 
 
-def parse_parts(packet, start, parts):
+def parse_parts(message, start, parts):
     part_start = start + 2
-    part_len = packet[start:part_start]
+    part_len = message[start:part_start]
 
     if len(part_len) == 0:
         return parts
 
     part_end = part_start + (int(part_len, 16) * 2)
-    parts.append(packet[part_start:part_end])
+    parts.append(message[part_start:part_end])
 
-    if packet[part_end:part_end + 2] == "00" or part_end > len(packet):
+    if message[part_end:part_end + 2] == "00" or part_end > len(message):
         return parts
     else:
-        return parse_parts(packet, part_end, parts)
+        return parse_parts(message, part_end, parts)
 
 
 if __name__ == '__main__':
@@ -88,9 +88,9 @@ if __name__ == '__main__':
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        sock.sendto(binascii.unhexlify(message), ("185.211.88.6", 53))
+        sock.sendto(binascii.unhexlify(message), ("8.8.8.8", 53))
         response, _ = sock.recvfrom(4096)
     finally:
         sock.close()
 
-    print("Response:\n" + decode_packet(binascii.hexlify(response).decode("utf-8")))
+    print(decode_packet(binascii.hexlify(response).decode("utf-8")))
